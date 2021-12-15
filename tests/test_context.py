ad8e4b63f5ffd76e8acae65a631281c942f025d2
Alex Grönholm <alex.gronholm@nextday.fi>
2021-12-15 11:44:33 +0200

    Merged test_context_py36 into test_context

--- tests/test_context.py
+++ tests/test_context.py
@@ -510,3 +510,27 @@ class TestContextTeardown:
             pass
 
         await start(Context())
+
+
+class TestContextFinisher:
+    @pytest.mark.parametrize('expected_exc', [
+        None, Exception('foo')
+    ], ids=['no_exception', 'exception'])
+    @pytest.mark.asyncio
+    async def test_context_teardown(self, expected_exc):
+        @context_teardown
+        async def start(ctx: Context):
+            nonlocal phase, received_exception
+            phase = 'started'
+            exc = yield
+            phase = 'finished'
+            received_exception = exc
+
+        phase = received_exception = None
+        context = Context()
+        await start(context)
+        assert phase == 'started'
+
+        await context.close(expected_exc)
+        assert phase == 'finished'
+        assert received_exception == expected_exc
