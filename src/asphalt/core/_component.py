0432c538af189ef22d9a3633e95f890aec4707a9
Alex Grönholm <alex.gronholm@nextday.fi>
2022-04-21 16:30:19 +0300

    Replaced old string substitution with f-strings

--- src/asphalt/core/_component.py
+++ src/asphalt/core/_component.py
@@ -145,12 +145,12 @@ class CLIApplicationComponent(ContainerComponent):
                 if 0 <= retval <= 127:
                     sys.exit(retval)
                 else:
-                    warn("exit code out of range: %d" % retval)
+                    warn(f"exit code out of range: {retval}")
                     sys.exit(1)
             elif retval is not None:
                 warn(
-                    "run() must return an integer or None, not %s"
-                    % qualified_name(retval.__class__)
+                    f"run() must return an integer or None, not "
+                    f"{qualified_name(retval.__class__)}"
                 )
                 sys.exit(1)
             else:
